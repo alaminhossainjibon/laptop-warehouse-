bfa0bda205c82f9abbacecb5bd5e7d49d2d3ae0c
alaminhossainjibon <mdalamin7622@gmail.com>
2022-05-19 15:21:08 +0600

    .env file

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJlfk047phu-uHNtQFMtuydU4qZeLkb28",
-    authDomain: "laptop-warehouse-78ab7.firebaseapp.com",
-    projectId: "laptop-warehouse-78ab7",
-    storageBucket: "laptop-warehouse-78ab7.appspot.com",
-    messagingSenderId: "725091382648",
-    appId: "1:725091382648:web:51a656473774c365e1048d"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
